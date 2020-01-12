6c793f686e3c0def45cdd1fb8d848f48429d3716
seethersan <carlos_jcez@hotmail.com>
2020-01-12 10:25:38 -0500

    refactoring to coomon crud interface. Closes #11

--- pet-clinic-data/src/main/java/dev/tokhna/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/dev/tokhna/sfgpetclinic/services/PetService.java
@@ -2,10 +2,5 @@ package dev.tokhna.sfgpetclinic.services;
 
 import dev.tokhna.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
