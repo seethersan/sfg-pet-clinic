fb3370a8f3f3d1d73e7895434d35761620b4516b
seethersan <carlos_jcez@hotmail.com>
2020-01-12 20:38:39 -0500

    Adding Pet List and updated controller.

--- pet-clinic-web/src/main/java/dev/tokhna/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/dev/tokhna/sfgpetclinic/controllers/PetController.java
@@ -1,13 +1,22 @@
 package dev.tokhna.sfgpetclinic.controllers;
 
+import dev.tokhna.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/pets")
 @Controller
 public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String petList() {
+    public String petList(Model model) {
+        model.addAttribute("pets", petService.findAll());
         return "pets/index";
     }
 }
