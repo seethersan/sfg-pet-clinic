4be763f256dd63d5c94ed82cebf7e594ec42ee88
seethersan <carlos_jcez@hotmail.com>
2020-01-19 18:29:13 -0500

    added PetTypeService. Closes #31.

---
+++ pet-clinic-data/src/main/java/dev/tokhna/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package dev.tokhna.sfgpetclinic.services;
+
+import dev.tokhna.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/dev/tokhna/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package dev.tokhna.sfgpetclinic.services.map;
+
+import dev.tokhna.sfgpetclinic.model.PetType;
+import dev.tokhna.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
