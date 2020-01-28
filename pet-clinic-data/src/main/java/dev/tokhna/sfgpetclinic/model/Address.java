609b0f11f30e0e058de8c1d218a8f5e6c0e74268
seethersan <carlos_jcez@hotmail.com>
2020-01-28 16:08:52 -0500

    Updated Vets with JPA annotation. Closes #38

--- pet-clinic-data/src/main/java/dev/tokhna/sfgpetclinic/model/Address.java
+++ pet-clinic-data/src/main/java/dev/tokhna/sfgpetclinic/model/Address.java
@@ -1,12 +1,19 @@
 package dev.tokhna.sfgpetclinic.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "address")
 public class Address extends BaseEntity {
+    @Column(name = "name")
     private String name;
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
 
     public String getName() {
